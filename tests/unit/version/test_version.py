6200c77f35649dc95b067e34c31b03072fff3d69
ShivanshSinghalIET <2200520100058@ietlucknow.ac.in>
2024-08-27 14:15:45 +0530

    Removed SDV

--- tests/unit/version/test_version.py
+++
@@ -1,8 +0,0 @@
-import sdv
-
-
-def test_sdv_versions():
-    """Test version for SDV."""
-    assert sdv.version.__all__ == ('public', 'enterprise')
-    assert sdv.version.public == sdv.__version__
-    assert sdv.version.enterprise is None
